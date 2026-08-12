9c7e13e8c9052b6594679690703c68ce0cff1430
darshan-gowdaa <darshangowdaa223@gmail.com>
2026-08-12 13:01:45 +0530

    chore: bump version to 1.0.3 and fix firefox build script

--- extension/build-firefox.ps1
+++ extension/build-firefox.ps1
@@ -10,7 +10,7 @@ Copy-Item manifest.firefox.json manifest.json -Force
 
 Remove-Item -Path temp_build -Recurse -Force -ErrorAction SilentlyContinue
 New-Item -ItemType Directory -Path temp_build | Out-Null
-Copy-Item -Path * -Destination temp_build -Recurse -Exclude "sidebar-app", "*.ps1", "*.sh", "*.zip", ".gitignore", "temp_build", "manifest.json.chrome-bak", "manifest.firefox.json", "background.js", "offscreen.js", "offscreen.html"
+Copy-Item -Path * -Destination temp_build -Recurse -Exclude "sidebar-app", "*.ps1", "*.sh", "*.zip", ".gitignore", "temp_build", "manifest.json.chrome-bak", "manifest.firefox.json", "offscreen.js", "offscreen.html"
 New-Item -ItemType Directory -Path temp_build/sidebar-app | Out-Null
 Copy-Item -Path sidebar-app/* -Destination temp_build/sidebar-app -Recurse -Exclude "node_modules", "src"
 
